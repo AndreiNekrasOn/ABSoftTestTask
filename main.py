0690b378aeb33ad895c979f6474e02c82429dda8
AndreiNekrasOn <an-32010@yandex.ru>
2019-06-24 02:42:19 +0300

    Added configuration file, code beautyfied

--- main.py
+++ main.py
@@ -1,85 +1,41 @@
 import multiprocessing as mp
 import os
 import ftplib
-
-ftps = ftplib.FTP('localhost')
-ftps.login('Andrei', '')
+import json
 
 
-def ftp_validate_dir(path):
+class ServerInfo(object):
+    def __init__(self, config_file):
+        with open(config_file) as cf:
+            server_info = json.loads(cf.read())
+        self.name = server_info['server_name']
+        self.username = server_info['user_name']
+        self.password = server_info['password']
+        self.local_paths = server_info['local_paths']
+        self.server_paths = server_info['server_paths']
+
+
+def ftp_makedir(path):
     try:
         ftps.mkd(os.path.dirname(path))
     except ftplib.error_perm:
         if len(os.path.dirname(path)):
-            print("logfile :: path is not a valid directory or directory already exists: /",
-                  os.path.dirname(path), sep='')
+            print("path is not a valid directory or directory already exists: ", os.path.dirname(path), sep='')
         else:
             pass
 
 
 def ftp_write(local_dir, ftp_dir):
-    ftp_validate_dir(ftp_dir)
+    ftp_makedir(ftp_dir)
     with open(local_dir, 'rb') as f:
         ftps.storbinary("STOR " + ftp_dir, f)
 
 
-if __name__ == '__main__':
-    srcs = [
-        'C:\\Users\\Андрей\\Desktop\\PythonPro\\ABSoftTestTask\\first.txt',
-        'C:\\Users\\Андрей\\Desktop\\PythonPro\\ABSoftTestTask\\second.txt',
-    ]
-    nsrcs = [
-        'helllo/new.txt',
-        'bye.txt',
-    ]
+server = ServerInfo("config.json")
+ftps = ftplib.FTP(server.name)
+ftps.login(server.username, server.password)
 
-    zipped_dirs = list(zip(srcs, nsrcs))
+if __name__ == '__main__':
+    zipped_dirs = zip(server.local_paths, server.server_paths)
     with mp.Pool() as pool:
         pool.starmap(ftp_write, zipped_dirs)
-
-
-# import multiprocessing as mp
-# import os
-# import ftplib
-#
-#
-# class FTPWriter:
-#     ftps = ftplib.FTP()
-#
-#     def __init__(self, address, username, password):
-#         ftps = ftplib.FTP(address)
-#         ftps.login(username, password)
-#
-#     def validate_dir(self, path):
-#         try:
-#             self.ftps.mkd(os.path.dirname(path))
-#         except ftplib.error_perm:
-#             if len(os.path.dirname(path)):
-#                 print("logfile :: path is not a valid directory or directory already exists: /",
-#                       os.path.dirname(path), sep='')
-#             else:
-#                 pass
-#
-#     def write(self, local_dir, ftp_dir):
-#         self.validate_dir(ftp_dir)
-#         with open(local_dir, 'rb') as f:
-#             self.ftps.storbinary("STOR " + ftp_dir, f)
-#
-#     def __del__(self):
-#         self.ftps.close()
-#
-#
-# if __name__ == '__main__':
-#     srcs = [
-#         'C:\\Users\\Андрей\\Desktop\\PythonPro\\ABSoftTestTask\\first.txt',
-#         'C:\\Users\\Андрей\\Desktop\\PythonPro\\ABSoftTestTask\\second.txt',
-#     ]
-#     nsrcs = [
-#         'helllo/new.txt',
-#         'bye.txt',
-#     ]
-#
-#     writer = FTPWriter('localhost', 'Andrei', '')
-#     zipped_dirs = list(zip(srcs, nsrcs))
-#     with mp.Pool() as pool:
-#         pool.starmap(writer.write, zipped_dirs)
